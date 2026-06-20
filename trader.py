71b5a8b52d1054e8dd384a45736c69bfcf2d8fe2
wally1324-lgtm <chegzami.walid01@icloud.com>
2026-06-20 14:41:14 +0200

    Create trader.py

---
+++ trader.py
@@ -0,0 +1,36 @@
+from data.market import Market
+from bot.strategy import generate_signal
+from bot.risk import position_size
+from bot.portfolio import Portfolio
+
+market = Market()
+portfolio = Portfolio()
+
+prices = []
+
+def run_trade():
+    price = market.tick()
+
+    prices.append(price)
+
+    signal = generate_signal(prices)
+
+    amount = position_size(
+        portfolio.balance,
+        price
+    )
+
+    if signal == "BUY":
+        portfolio.buy(price, amount)
+
+    elif signal == "SELL":
+        portfolio.sell(price, amount)
+
+    portfolio_value = portfolio.value(price)
+
+    return (
+        price,
+        signal,
+        amount,
+        portfolio_value
+    )
